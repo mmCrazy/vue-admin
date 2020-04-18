9e79031e23eee902cd9de9258f0c62ad5ed64ce1
mmCrazy <747304031@qq.com>
2020-04-18 18:23:03 +0800

    配置完成

---
+++ src/views/Login/login.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div id="login">
+        1121313
+    </div>
+</template>
+<script>
+export default {
+    name: "login"
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
